390a4e838ab6b0e2e6f73a8f213862f11987b47c
Sekolah <adriangautama73@gmail.com>
2026-01-28 09:53:31 +0800

    good enough

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import random
 
-
-user = int(input("Masukkan angka!: "))
-percobaan = 0
+print("""Pilih mode:
+      1.MUUDDAH (10 Kali Percobaan)
+      2.BIASA AJ (5 Kali Percobaan)
+      3.ARGHHHH (3 Kali Percobaan)""")
+tingkatkatan = int(input("Pilih tingkat kesulitan (Ketik Nomor): "))
+if tingkatkatan == 1:
+    percobaan = 10
+elif tingkatkatan == 2:
+    percobaan = 5
+else:
+    percobaan = 3
+user = 0
+end_msg = "SIKIL ISU (CUPU)"
 jawaban = random.randint(1, 10)
+i = 0
 print(jawaban)
-
-while jawaban != user:
+while i < percobaan:
+    i += 1
+    user = int(input("Masukkan angka!: "))
     if user > jawaban:
         print("jawaban anda lebih besar dari pada jawaban")
-    if user < jawaban:
+    elif user < jawaban:
         print("jawaban anda lebih kecil dari pada jawaban")
-    user = int(input("Masukkan angka!: "))
+    elif jawaban == user :
+        end_msg = "KAMU MENANG!!"
+        break
 
-print("kamu menang!!!")
 
+print(end_msg)

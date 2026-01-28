6d2d5b59ad2dbca9f3bb7d107fce54f9f82e06c2
Sekolah <adriangautama73@gmail.com>
2026-01-28 08:58:48 +0800

    gameloop

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import random
 
 
-user = input("Masukkan angka!: ")
+user = int(input("Masukkan angka!: "))
 percobaan = 0
 jawaban = random.randint(1, 10)
 print(jawaban)
 
-while jawaban != int(user):
-    user = input("Masukkan angka!: ")
+while jawaban != user:
+    if user > jawaban:
+        print("jawaban anda lebih besar dari pada jawaban")
+    if user < jawaban:
+        print("jawaban anda lebih kecil dari pada jawaban")
+    user = int(input("Masukkan angka!: "))
 
+print("kamu menang!!!")
 
